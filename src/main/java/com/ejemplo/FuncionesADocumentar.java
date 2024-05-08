0afe602b22bb2959f973db25064de8e62ffad954
ik012982i9 <a@a.a>
2024-05-08 09:32:03 +0200

    Creacion de la documentacion

--- src/main/java/com/ejemplo/FuncionesADocumentar.java
+++ src/main/java/com/ejemplo/FuncionesADocumentar.java
@@ -1,6 +1,12 @@
 package com.ejemplo;
-
+/**
+ * Esta clase contiene métodos para realizar operaciones  en arrays de enteros.
+ */
 public class FuncionesADocumentar {
+    /**
+     * Método principal que demuestra el uso de los métodos de esta clase.
+     * @param args argumentos de la línea de comandos (no se ha utilizado)
+     */
     public static void main(String[] args) {
         
         int a[] = {7, 14, 9, 18, 11, 6, 13, 12, 1, 5, 4, 3, 2, 8, 10};
@@ -15,6 +21,11 @@ public class FuncionesADocumentar {
         ordenar(a);
     }
 
+    /**
+     * Calcula la suma de los elementos en un array.
+     * @param a array de enteros
+     * @return La suma de los elementos del array
+     */
     public static int suma(int a[]) {
         int suma = 0;
         for (int i = 0; i < a.length; i++)
@@ -22,10 +33,20 @@ public class FuncionesADocumentar {
         return suma;
     }
 
+    /**
+     * Calcula el promedio de los elementos en un array.
+     * @param a array de enteros
+     * @return El promedio de los elementos del array
+     */
     public static double promedio(int a[]) {
         return suma(a) / a.length;
     }
 
+    /**
+     * Encuentra el valor máximo en un array de enteros.
+     * @param a array de enteros
+     * @return El mayor elemento del array
+     */
     public static int mayor(int a[]) {
         int mayor = a[0];
         for (int i = 1; i < a.length; i++)
@@ -34,6 +55,11 @@ public class FuncionesADocumentar {
         return mayor;
     }
 
+    /**
+     * Encuentra el valor mínimo en un array de enteros.
+     * @param a array de enteros
+     * @return El menor elemento del array
+     */
     public static int menor(int a[]) {
         int menor = a[0];
         for (int i = 1; i < a.length; i++)
@@ -42,12 +68,21 @@ public class FuncionesADocumentar {
         return menor;
     }
 
+    /**
+     * Invierte un array de enteros e imprime los elementos invertidos
+     * @param a array de enteros a invertir
+     */
+
     public static void invertir(int a[]) {
         for (int i = a.length - 1; i >= 0; i--)
             System.out.print(a[i] + " ");
         System.out.println();
     }
 
+    /**
+     * Ordena un array de enteros en orden ascendente e imprime los elementos ordenados
+     * @param a array de enteros a ordenar
+     */
     public static void ordenar(int a[]) {
         int aux;
         for (int i = 0; i < a.length - 1; i++)
--- src/main/java/com/ejemplo/clases/Alumno.java
+++ src/main/java/com/ejemplo/clases/Alumno.java
@@ -1,20 +1,33 @@
 package com.ejemplo.clases;
-
+/**
+ * Clase que representa a un alumno.
+ */
 public class Alumno {
     private String identificador;
     private String nombre;
     private String curso;
-
+    /**
+     * Constructor de la clase Alumno.
+     * @param identificador el identificador del alumno.
+     * @param nombre el nombre del alumno.
+     * @param curso el curso en el que está el alumno.
+     */
     // Constructor
     public Alumno(String identificador, String nombre, String curso) {
         this.identificador = identificador;
         this.nombre = nombre;
         this.curso = curso;
     }
-
+    /**
+     * Método para matricular al alumno en un nuevo curso.
+     * @param curso el nuevo curso en el que se matricula el alumno.
+     */
     public void matricular(String curso){
         this.curso = curso + " 1";
     }
+    /**
+     * Método para que el alumno pase al siguiente curso o se titule.
+     */
 
     public void pasarDeCurso(){
         String[] curso = this.curso.split(" ");
@@ -28,28 +41,46 @@ public class Alumno {
         }
 
     }
-
+    /**
+     * Obtiene el identificador del alumno.
+     * @return el identificador del alumno.
+     */
     // Getters y Setters
     public String getIdentificador() {
         return identificador;
     }
-
+    /**
+     * Establece el identificador del alumno.
+     * @param identificador el nuevo identificador del alumno.
+     */
     public void setIdentificador(String identificador) {
         this.identificador = identificador;
     }
-
+    /**
+     * Obtiene el nombre del alumno.
+     * @return el nombre del alumno.
+     */
     public String getNombre() {
         return nombre;
     }
-
+    /**
+     * Establece el nombre del alumno.
+     * @param nombre el nuevo nombre del alumno.
+     */
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
-
+    /**
+     * Obtiene el curso del alumno.
+     * @return el curso del alumno.
+     */
     public String getCurso() {
         return curso;
     }
-
+    /**
+     * Establece el curso del alumno.
+     * @param curso el nuevo curso del alumno.
+     */
     public void setCurso(String curso) {
         this.curso = curso;
     }
